f724a906a5247f016976d676642c5bd2af4dd716
Michael Farmer <mfarmer91@gmail.com>
2017-07-20 10:35:47 -0400

    updated quiz.js with new funtion buildList, and app now prints as ordered list.

--- quiz.js
+++ quiz.js
@@ -5,15 +5,26 @@ var quiz = [
 ];
 
 var correctQuestions = [];
-
 var incorrectQuestions = [];
 
 var correct = 0;
 var incorrect = 0;
 
+var html;
+
+
 function print ( message ) {
     var outputDiv = document.getElementById( 'output' );
     outputDiv.innerHTML = message;
+}
+
+function buildList ( arr ) {
+    var htmlList = '<ol>';
+    for ( var i = 0; i < arr.length; i++ ){
+        htmlList += '<li>' + arr[i] + '</li>';
+    } 
+    htmlList += '</ol>';
+    return htmlList;
 }
 
 
@@ -23,20 +34,22 @@ function quizAsk ( list ) {
         
         if ( answer === quiz[i][1] ) {
             correctQuestions.push(quiz[i][0]);
-            correct++;
-            alert("Noice! You're slangin' design knowledge like it's nothin', stretch(er)!");
-            
+            correct++;  
         } else {
-            alert("That's okay.  Charles and Ray Eames made a plywood leg splint that you can use to support your injured design knowledge.")
             incorrectQuestions.push(quiz[i][0]);
             incorrect++;
         }
     }
-        print('Congrats! You answered ' + correct + ' furniture design questions correctly!' + '<br>' + '<br>' + 'These were the joints you answered correctly:' + '<br>' + correctQuestions + '<br>' + '<br>' + 'These joints burned a little bit more for ya:' + '<br>' + incorrectQuestions );
-//        print( incorrect + ' incorrect. Hmm. Hey, not great, but certainly Mies Van Der Rohe wasnt able to crank out the Barcalona chair on his first go! ' + '<br>' );
 }
 
 quizAsk( quiz );
+
+html = 'Ay, you answered ' + correct + ' furniture design question(s) correctly!'
+html += '<h2>You got these joints correct:</h2>' + buildList(correctQuestions);
+html += '<h2>You got these joints incorrect:</h2>' + buildList(incorrectQuestions);
+print( html );
+
+
 
     
     
